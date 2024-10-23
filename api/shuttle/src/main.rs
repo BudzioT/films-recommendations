66444a1b76f9c7cba9439d54015f1b06e62831ea
BudzioT <budzio.zsm1@gmail.com>
2024-10-23 18:13:16 +0200

    Set up database

--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -1,4 +1,6 @@
 use axum::{routing::get, Router};
+use shuttle_runtime::CustomError;
+use sqlx::Executor;
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
@@ -8,6 +10,12 @@ async fn hello_world() -> &'static str {
 async fn main(
     #[shuttle_shared_db::Postgres] pool: sqlx::PgPool
 ) -> shuttle_axum::ShuttleAxum {
+    // Execute schema sql file
+    pool.execute(include_str!("../../db/schema.sql"))
+        .await
+        .map_err(CustomError::new)?;
+
+    // Root router
     let router = Router::new().route("/", get(hello_world));
 
     Ok(router.into())
